bf5c5c2d549d20f018a89eba891f9d56c8307a4e
hamizan18 <hamizanputra321@gmail.com>
2026-05-14 17:23:04 +0700

    gaa flip + open lebih fast

--- HaloCV/ExplainTask1.md
+++ HaloCV/ExplainTask1.md
@@ -1,3 +1,12 @@
 success, frame = camrea.read()
 success -> berhasil baca atau tidak
 fram -> gambar dari kamera
+
+if cv2.waitKey(1) == 27:
+27 = tombol ESC
+pake ord('button_here') kalo mau pake tombol selain ESQ
+
+frame = cv2.flip(frame, 1) ubah gini biar gaa mirror di Windows
+
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW) ini fungsinya biar lebih cepat munculnya
+
--- HaloCV/main.py
+++ HaloCV/main.py
@@ -1,14 +1,29 @@
 import cv2
 
-camera = cv2.VideoCapture(0)
+camera = cv2.VideoCapture(0, cv2.CAP_DSHOW)
 
 while True:
     success, frame = camera.read()
-
-    cv2.imshow("My Camera", frame)
-
-    if cv2.waitKey(1) == 27:
+    frame = cv2.flip(frame, 1)
+    
+    if not success:
         break
+
+    cv2.putText(
+        frame, # Frame tujuan
+        "COMPUTER VISION STARTED", # Teks yang munculs
+        (20, 50), # Posisi Teks (x, y)
+        cv2.FONT_HERSHEY_COMPLEX, # Font
+        1, # Ukuran
+        (0, 255, 0), # Warna Font
+        2 # Ketebalan
+    )
+    
+    cv2.imshow("MIJAAN VISION", frame)
+
+    if cv2.waitKey(1) == ord('q'):
+        break
+    
     
 camera.release()
 cv2.destroyAllWindows()
